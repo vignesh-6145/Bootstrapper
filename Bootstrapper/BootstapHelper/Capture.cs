d5240f999dcf961b47dc8e7320f0046f62c38774
vignesh.bandla <vignesh.bandla01@gmail.com>
2023-10-12 23:33:59 +0530

    UPDATE : added the Welcome string and a option

---
+++ Bootstrapper/BootstapHelper/Capture.cs
@@ -0,0 +1,23 @@
+﻿using System.CommandLine;
+namespace BootstapHelper
+{
+    public class Capture
+    {
+        // starts capturing the user inputs from CLI and also displays the home screen
+        public void start(string[] args)
+        {
+             
+            var langOption = new Option<string>("-l");
+            langOption.AddAlias("--lang");
+            langOption.Description = "Use this option to specify which programming language you want to choose";
+            
+
+
+            var rootCommand = new RootCommand("Welcome to Bootstrapper.exe");
+            rootCommand.Add(langOption);
+            rootCommand.SetHandler((string langValue) => { Console.WriteLine($"langValue : {langValue}"); }, langOption);
+            rootCommand.Invoke(args);
+        }
+
+    }
+}
