cd78cd95f911cb576aa2a8474d54ad2e0de35b35
vignesh.bandla <vignesh.bandla01@gmail.com>
2023-10-16 03:29:35 +0530

    UPDATE : Options and Argument info was added

--- Bootstrapper/BootstapHelper/Capture.cs
+++ Bootstrapper/BootstapHelper/Capture.cs
@@ -9,13 +9,43 @@ namespace BootstapHelper
              
             var langOption = new Option<string>("-l");
             langOption.AddAlias("--lang");
+            langOption.Arity = ArgumentArity.ExactlyOne;
             langOption.Description = "Use this option to specify which programming language you want to choose";
+            langOption.IsRequired = true;
+
+            var classOption = new Option<string>("-c");
+            classOption.AddAlias("--class");
+            classOption.Arity = ArgumentArity.ExactlyOne;
+            classOption.Description = "This option was used to name the file";
+
+            var isMainOption = new Option<bool>("-m");
+            isMainOption.AddAlias("--main");
+            isMainOption.Arity = ArgumentArity.ZeroOrOne;
+            isMainOption.Description = "This specifies whether the class contains a main method or not";
+
+            var packagesOption = new Option<string[]>("-p");
+            packagesOption.AddAlias("--packages");
+            packagesOption.Arity = ArgumentArity.ZeroOrMore;
+            packagesOption.Description="Specify the packages you require to import";
+
             
 
 
             var rootCommand = new RootCommand("Welcome to Bootstrapper.exe");
+
             rootCommand.Add(langOption);
-            rootCommand.SetHandler((string langValue) => { Console.WriteLine($"langValue : {langValue}"); }, langOption);
+            rootCommand.Add(classOption);
+            rootCommand.Add(isMainOption);
+            rootCommand.Add(packagesOption);
+
+            rootCommand.SetHandler((langValue, classValue, isMainValue, packagesValue) 
+                                   => { 
+                                        Console.WriteLine($"langValue : {langValue}");
+                                        Console.WriteLine($"classValue : {classValue}");
+                                        Console.WriteLine($"isMainValue : {isMainValue}");
+                                       Console.WriteLine($"packagesValue : {packagesValue.Count()}");
+                                    }
+                                    , langOption,classOption,isMainOption,packagesOption);
             rootCommand.Invoke(args);
         }
 
